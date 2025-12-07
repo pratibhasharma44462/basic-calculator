1fe90546b2df3032885e327fc977789793506f45
PRATIBHA SHARMA <pratibhamafia345@gmail.com>
2025-12-08 00:59:02 +0530

    Add files via upload

---
+++ normal calculator.py
@@ -0,0 +1,60 @@
+# calculator
+logo = r"""
+ _____________________
+|  _________________  |
+| | Pythonista   0. | |  .----------------.  .----------------.  .----------------.  .----------------. 
+| |_________________| | | .--------------. || .--------------. || .--------------. || .--------------. |
+|  ___ ___ ___   ___  | | |     ______   | || |      __      | || |   _____      | || |     ______   | |
+| | 7 | 8 | 9 | | + | | | |   .' ___  |  | || |     /  \     | || |  |_   _|     | || |   .' ___  |  | |
+| |___|___|___| |___| | | |  / .'   \_|  | || |    / /\ \    | || |    | |       | || |  / .'   \_|  | |
+| | 4 | 5 | 6 | | - | | | |  | |         | || |   / ____ \   | || |    | |   _   | || |  | |         | |
+| |___|___|___| |___| | | |  \ `.___.'\  | || | _/ /    \ \_ | || |   _| |__/ |  | || |  \ `.___.'\  | |
+| | 1 | 2 | 3 | | x | | | |   `._____.'  | || ||____|  |____|| || |  |________|  | || |   `._____.'  | |
+| |___|___|___| |___| | | |              | || |              | || |              | || |              | |
+| | . | 0 | = | | / | | | '--------------' || '--------------' || '--------------' || '--------------' |
+| |___|___|___| |___| |  '----------------'  '----------------'  '----------------'  '----------------' 
+|_____________________|
+"""
+print(logo)
+restart = True
+
+first_number = float(input("write the first number:" ))
+while restart:
+    print("+\n-\n*\n/\n**'power'\n")
+    operation = input("Pick an operation you want to perform?: ")
+
+    if operation not in ["+", "-", "*", "/", "**"]:
+        print("wrong input")
+        continue
+
+    second_number = float(input("Write the other number: "))
+
+
+    if operation == "+":
+        result = (first_number + second_number)
+    elif operation == "-":
+        result = (first_number - second_number)
+    elif operation == "*":
+        result = (first_number * second_number)
+    elif operation == "/":
+        result = (first_number / second_number)
+    elif operation == "**":
+        result = (first_number ** second_number)
+    else:
+        print("wrong input")  
+
+    print(f"result:{result} ")
+
+
+    again = (input("type 'yes' if you want to continue with this or 'no' for exit: "))   
+    if again == 'yes':
+        first_number = result
+    if again == 'no':
+        restart = False
+        print("Thanks for using!!🥰") 
+       
+
+     
+
+
+
